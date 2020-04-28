fc9deb2083cc84303ad57764ed9e1c0927c4f604
Ricardo Tondello <rkdtondello@gmail.com>
2020-04-28 04:56:40 -0300

    CRUP Department by scaffolding and migration

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
 
 namespace SalesWebMvc {
     public class Startup {
@@ -20,6 +22,9 @@ namespace SalesWebMvc {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddControllersWithViews();
+
+    services.AddDbContext<SalesWebMvcContext>(options =>
+            options.UseSqlite(Configuration.GetConnectionString("SalesWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
